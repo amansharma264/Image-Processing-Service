e841ddafc608742e252d055cf19898fe6b34a49a
amansharma264 <amankumar170789@gmail.com>
2025-10-05 01:39:27 +0530

    fix: Restore dotenv configuration to ensure environment variables are loaded correctly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 // NOTE: Environment variables are loaded automatically via the 'nodemon -r dotenv/config' script
 // in package.json. Removed the redundant 'dotenv.config()' and its import.
 
